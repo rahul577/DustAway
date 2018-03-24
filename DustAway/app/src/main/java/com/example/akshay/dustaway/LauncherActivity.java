19fd7e1bc79efa202bc2e46206124bf774c95306
rahul577 <rahulbafila7@gmail.com>
2018-03-24 08:03:17 +0530

    first commit by rahul

---
+++ DustAway/app/src/main/java/com/example/akshay/dustaway/LauncherActivity.java
@@ -0,0 +1,107 @@
+package com.example.akshay.dustaway;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.ibm.watson.developer_cloud.conversation.v1.ConversationService;
+import com.ibm.watson.developer_cloud.conversation.v1.model.MessageRequest;
+import com.ibm.watson.developer_cloud.conversation.v1.model.MessageResponse;
+import com.ibm.watson.developer_cloud.http.ServiceCallback;
+
+import co.intentservice.chatui.ChatView;
+import co.intentservice.chatui.models.ChatMessage;
+
+public class LauncherActivity extends AppCompatActivity {
+
+    Button cycle;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launcher);
+
+        cycle = (Button) findViewById(R.id.cycle);
+        final ConversationService myConversationService =
+                new ConversationService(
+                        "2017-05-26",
+                        getString(R.string.username),
+                        getString(R.string.password)
+                );
+
+
+        final ChatView chatView = (ChatView) findViewById(R.id.chat_view);
+        chatView.addMessage(new ChatMessage("Message received", System.currentTimeMillis(), ChatMessage.Type.RECEIVED));
+        chatView.setOnSentMessageListener(new ChatView.OnSentMessageListener() {
+            @Override
+            public boolean sendMessage(ChatMessage chatMessage) {
+                final String inputText = chatMessage.getMessage();
+                Log.d("msg",inputText);
+                MessageRequest request = new MessageRequest.Builder()
+                        .inputText(inputText)
+                        .build();
+
+                myConversationService
+                        .message(getString(R.string.workspace), request)
+                        .enqueue(new ServiceCallback<MessageResponse>() {
+                            @Override
+                            public void onResponse(MessageResponse response) {
+                                // More code here
+                                final String outputText = response.getText().get(0);
+
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        chatView.addMessage(new ChatMessage(outputText, System.currentTimeMillis(), ChatMessage.Type.RECEIVED));
+                                    }
+                                });
+                            }
+
+                            @Override
+                            public void onFailure(Exception e) {}
+                        });
+
+
+
+                return true;
+            }
+        });
+
+        chatView.setTypingListener(new ChatView.TypingListener() {
+            @Override
+            public void userStartedTyping() {
+
+            }
+
+            @Override
+            public void userStoppedTyping() {
+
+            }
+        });
+
+        cycle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(LauncherActivity.this, cycleActivity.class);
+                startActivity(i);
+            }
+        });
+
+
+    }
+
+
+
+
+
+}
